a0e9017ea302d306a696d02b0c097df753f16c07
EagleCoder404 <harsha.jediknight@gmail.com>
2020-07-06 17:29:53 +0530

    changed something

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "this is very very secret"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("CODEZILLA_DATABASE_URL") or "sqlite:///"+ os.path.join(basedir,"app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///"+ os.path.join(basedir,"app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
